98fa9b998b4a3a5e2f4df7be118142349105a550
Freddy Lopez <freddlopez01@gmail.com>
2024-08-08 16:16:26 -0700

    adding old project repo

---
+++ lr.py
@@ -0,0 +1,134 @@
+#!/usr/bin/python
+#
+# CIS 472/572 - Logistic Regression Template Code
+#
+# Author: Daniel Lowd <lowd@cs.uoregon.edu>
+# Date:   2/9/2018
+#
+# Please use this code as the template for your solution.
+#
+import sys
+import re
+from math import log
+from math import exp
+from math import sqrt
+
+MAX_ITERS = 100
+
+
+# Load data from a file
+def read_data(filename):
+    f = open(filename, 'r')
+    p = re.compile(',')
+    data = []
+    header = f.readline().strip()
+    varnames = p.split(header)
+    namehash = {}
+    for l in f:
+        example = [int(x) for x in p.split(l.strip())]
+        x = example[0:-1]
+        y = example[-1]
+        data.append((x, y))
+    return (data, varnames)
+
+def sigmoid(z):
+    return 1/(1+exp(-z))
+
+
+# Train a logistic regression model using batch gradient descent
+def train_lr(data, eta, l2_reg_weight):
+    numvars = len(data[0][0])
+    w = [0.0] * numvars
+    b = 0.0
+
+    for i in range(MAX_ITERS):
+        gradient_w = [0.0] * numvars
+        gradient_b = 0.0
+        
+        # iterate over the data for every epoch 
+        for x, y in data:
+            z = 0
+            # calculate dot product of xi and wi 
+            for xi, wi in zip(x, w): 
+                z += (xi * wi)
+            z += b # add the bias 
+            z = sigmoid(z) # perform the sigmoid operation on zz 
+            #print(f"z: {z}")
+            error = 0
+            # After struggling for a while, I noticed that the subtracting -1 from z would actually result in an addition and it was
+            # messing with the model. So Simon and I figured out that if we add 0 instead of -1 and it fixed the errors we had on out model 
+            if y == 1:
+                error = z - y
+            else: 
+                error = z - 0
+            for i in range(numvars): # compute gradient vector with respect to x
+                gradient_w[i] += error * x[i]
+                #print(f"gradient_w[i]: {gradient_w[i]}")
+            gradient_b += error 
+
+        for i in range(numvars):
+            gradient_w[i] += (l2_reg_weight * w[i])
+        #print(f"gradient_b: {gradient_b}")
+
+        # Update weights and bias
+        for i in range(numvars):
+            w[i] -= eta * gradient_w[i]
+        b -= eta * gradient_b
+
+        tmpSum = sum(gradient_w[i] ** 2 for i in range(numvars))
+        grad_magnitude = sqrt(tmpSum)
+        #print("here")
+        if grad_magnitude < 0.0001:
+            #print(f"Converged after {it+1} iterations")
+            break
+
+    return (w, b)
+
+
+# Predict the probability of the positive label (y=+1) given the
+# attributes, x.
+def predict_lr(model, x):
+    (w, b) = model
+    # YOUR CODE HERE
+    z = b
+    for xi, wi in zip(x, w):
+        z += xi * wi
+
+    sig = sigmoid(z)#
+
+    return sig # This is an random probability, fix this according to your solution
+
+
+# Load train and test data.  Learn model.  Report accuracy.
+def main(argv):
+    if (len(argv) != 5):
+        print('Usage: lr.py <train> <test> <eta> <lambda> <model>')
+        sys.exit(2)
+    (train, varnames) = read_data(argv[0])
+    (test, testvarnames) = read_data(argv[1])
+    eta = float(argv[2])
+    lam = float(argv[3])
+    modelfile = argv[4]
+
+    # Train model
+    (w, b) = train_lr(train, eta, lam)
+
+    # Write model file
+    f = open(modelfile, "w+")
+    f.write('%f\n' % b)
+    for i in range(len(w)):
+        f.write('%s %f\n' % (varnames[i], w[i]))
+
+    # Make predictions, compute accuracy
+    correct = 0
+    for (x, y) in test:
+        prob = predict_lr((w, b), x)
+        #print(prob)
+        if (prob - 0.5) * y > 0:
+            correct += 1
+    acc = float(correct) / len(test)
+    print("Accuracy: ", acc)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ perceptron.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python
+#
+import sys
+import re
+from math import log
+from math import exp
+
+MAX_ITERS = 100
+
+
+# Load data from a file
+def read_data(filename):
+    f = open(filename, 'r')
+    p = re.compile(',')
+    data = []
+    header = f.readline().strip()
+    varnames = p.split(header)
+    namehash = {}
+    for l in f:
+        example = [int(x) for x in p.split(l.strip())]
+        x = example[0:-1]
+        y = example[-1]
+        # Each example is a tuple containing both x (vector) and y (int)
+        data.append((x, y))
+    return (data, varnames)
+
+
+# Learn weights using the perceptron algorithm
+def train_perceptron(data):
+    # Initialize weight vector and bias
+    numvars = len(data[0][0])
+    w = [0.0] * numvars
+    b = 0.0
+
+    #
+    # YOUR CODE HERE!
+    count = 0
+    for num in range(MAX_ITERS):
+        for x, y in data:
+            d_prod = 0.0  #  # dot product variable that will be computed for every i in row[0] and each corresponding weight
+            #print(w)
+            for wi, xi in zip(w, x):
+                d_prod += wi * xi
+            d_prod += b
+            #print(f"d_prod: ------------------ {d_prod}")
+
+            #predict = 1 if d_prod >= 0 else -1 # I set predict to be 1 if the dot product from the previous loop is greater or equal to 0. Else, predict = -1
+            #print(f"Predict: {predict}       expected: {row[1]}")
+            #print(w)
+            if y * d_prod <= 0: # In the context of the training data, check if predict is the same as the predeterminded target attribute value 
+                #print("---------Values did not match. Error occured")
+                # if the statement evaluates to true, then update weights and add bias for updated bias 
+                for j in range(numvars):
+                    #print(w)
+                    w[j] += x[j] * y
+                old_b = b
+                b += y
+                #print(f"New bias: {b}     Old bias: {old_b}")
+
+    return (w, b)
+
+
+# Compute the activation for input x.
+# (NOTE: This should be a real-valued number, not simply +1/-1.)
+def predict_perceptron(model, x):
+    (w, b) = model
+
+    weight_sum = 0.0
+
+    for wi, xi, in zip(w, x):
+        weight_sum += wi * xi
+
+    weight_sum += b
+    #
+    # YOUR CODE HERE!
+    #
+
+    return weight_sum
+
+
+# Load train and test data.  Learn model.  Report accuracy.
+def main(argv):
+    # Process command line arguments.
+    # (You shouldn't need to change this.)
+    if (len(argv) != 3):
+        print('Usage: perceptron.py <train> <test> <model>')
+        sys.exit(2)
+    (train, varnames) = read_data(argv[0])
+    (test, testvarnames) = read_data(argv[1])
+    modelfile = argv[2]
+
+    # Train model
+    (w, b) = train_perceptron(train)
+
+    # Write model file
+    # (You shouldn't need to change this.)
+    f = open(modelfile, "w+")
+    f.write('%f\n' % b)
+    for i in range(len(w)):
+        f.write('%s %f\n' % (varnames[i], w[i]))
+
+    # Make predictions, compute accuracy
+    correct = 0
+    for (x, y) in test:
+        activation = predict_perceptron((w, b), x)
+        #print(activation)
+        if activation * y > 0:
+            correct += 1
+    acc = float(correct) / len(test)
+    print("Accuracy: ", acc)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
